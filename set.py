00ec9b9afa4ac388d4a10c86b540d175b69a1620
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-28 23:23:10 -0700

    Add files via upload

---
+++ set.py
@@ -0,0 +1,44 @@
+# m={8,9,6}
+# c='Hi',8,9,4
+# m.add(c)
+# print(m) # add 
+
+# r={1,2,3}
+# r.clear()
+# print(r)
+
+# a={5,6,3}
+# b={9,6,5}
+# c={7,8,4}
+
+
+# a={2.56356,2,4,3.4,'hii',(3,4),True,False}
+# b={False,3.4,'hii'}
+# c=a.difference(b)
+# print(c)
+
+# a={5,6,4}
+# b={6,3,2}
+# c={5,3,2}
+# a.update(b)
+# b.update(c)
+# print(a)
+
+# a={5,10,20,25,30}
+# b={10,21,5}
+# print(a.difference(b)) 
+# print(b.difference(a)) 
+# print(a-b)
+# # print(b-a)
+
+# z={35,47,96,73,40,23}
+# z.discard(44)
+# print(z)
+# z.discard(23)
+# print(z)
+
+
+# v={26,9,11,7,23}
+# v.pop()
+# print(v)
+
